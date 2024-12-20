4cdc13d82fa2d88fb758a05bfaec5a62894e7a17
sei-12 <sei.t11@icloud.com>
2024-12-20 10:31:56 +0900

    connect front <-> back

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app: express.Express = express()
+const port = 3001
+
+app.use(cors())
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
